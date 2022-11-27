ab93a0b6a924697c45811730fd8f34c23e5b8973
samuel burac <samuelburac@gmail.com>
2022-11-26 18:17:15 -0800

    little bit of cleaning

--- app/src/main/java/com/zybooks/todoit/MainActivity.java
+++ app/src/main/java/com/zybooks/todoit/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.clear_button).setOnClickListener(view -> clearButtonClick());
+
 
         mToDoList = new TaskList(this);
 
